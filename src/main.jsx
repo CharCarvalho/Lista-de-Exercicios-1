3f8f26f912c4ddf6e408143a7cef07469fc9f9ab
CharCarvalho <rm550113@fiap.com.br>
2023-09-06 19:55:58 -0300

    importações na main

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import Produtos from './routes/Produtos/Produtos.jsx'
+import Error from './routes/Error/Erro404.jsx'
+import EditarProduto from './routes/EditarProduto/EditarProduto.jsx'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
