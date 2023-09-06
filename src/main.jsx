dbb0ea09f388aaca8503adde7267cbb553edd51b
CharCarvalho <rm550113@fiap.com.br>
2023-09-06 20:00:50 -0300

    Mudanças na main

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import EditarProduto from './routes/EditarProduto/EditarProduto.jsx'
+import Error from './routes/Error/Erro404.jsx'
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/Produtos.jsx'
-import Error from './routes/Error/Erro404.jsx'
-import EditarProduto from './routes/EditarProduto/EditarProduto.jsx'
 
-
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path:"/",
+        element:<Home />
+        
+      },
+      {
+        path:"/produtos",
+        element: <Produtos/>
+      },
+      {path:'/editar/:id',
+      element:<EditarProduto />
+      }
+    ]
+  }])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
   </React.StrictMode>,
 )
