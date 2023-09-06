3b938f5c823d46dd74d6b374e74aead0b134e022
CharCarvalho <rm550113@fiap.com.br>
2023-09-06 20:27:34 -0300

    Criando ListaProdutos

---
+++ src/components/ListaProdutos.jsx
@@ -0,0 +1,8 @@
+export const ListaProdutos = [
+
+    {id:1, nome:"PenDrive", preco: 80},
+    {id:2, nome:"Teclado",preco: 79},
+    {id:3, nome:"Cadeira Gmer", preco: 1500},
+    {id:4, nome:"Monitor", preco: 1500},
+
+];
